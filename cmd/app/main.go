c3eabe0d0348d7faad386bbb487fcab76ae78ac3
undndnwnkk <bykov.lev74@gmail.com>
2026-06-06 12:37:20 +0300

    feat(auth-users-api): MG-3

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", healthHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
